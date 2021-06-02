55b60520b9b61b4025b7a28c91bd8b4bb688349d
Vladimir <bakzzzbunny@gmail.com>
2021-06-02 22:28:54 +0300

    ex3 complete

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,14 @@
 function compareArrays(arr1, arr2) {
   let result;
 
-  // Ваш код
+  if (arr1.length != arr2.length) {
+    return false;
+  }
+
+  arr1.filter(function(cv, index, arr) {
+    result = cv == arr2[index];
+    return result;
+  });
 
   return result; // boolean
 }
@@ -9,7 +16,11 @@ function compareArrays(arr1, arr2) {
 function advancedFilter(arr) {
   let resultArr;
 
-  // Ваш код
+  resultArr = arr.filter(function (cv, idx, arr) {
+    return cv > 0 && cv % 3 == 0;
+  }).map(function (cv, idx, arr) {
+    return cv * 10;
+  });
 
   return resultArr; // array
 }
