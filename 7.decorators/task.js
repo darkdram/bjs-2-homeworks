2286731f410c4425a90af879b65ed9a55deae345
Vladimir <bakzzzbunny@gmail.com>
2021-06-15 14:25:19 +0300

    ex 7

--- 7.decorators/task.js
+++ 7.decorators/task.js
@@ -1,5 +1,20 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+  let cache = {};
+  function wrapper(...args) {
+    let hash = args.join(',');
+    if (hash in cache) { // если значение уже присутствует в кэше, то отдаем его
+      return 'Из кэша: ' + cache[hash];
+    } else {
+      let result = func.call(this, ...args); // иначе считаем результат
+      let _keys = Object.keys(cache); 
+      if (_keys.length == 5) { // если длина кэша достигла максимального значения
+        delete cache[_keys[0]]; // удаляем первое значение, добавленное в кэш, освобождая место для нового
+      }
+      cache[hash] = result;
+      return 'Вычисляем: ' + result;
+    }
+  }
+  return wrapper;
 }
 
 
